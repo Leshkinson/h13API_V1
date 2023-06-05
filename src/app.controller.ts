73ce6efd78f54a38d9af28bcf6cd42b6ff514370
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-06-05 15:27:05 +0300

    fix11

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Delete } from "@nestjs/common";
+import { Controller, Delete, HttpStatus, Res } from "@nestjs/common";
 import { BlogsService } from "./blogs/blogs.service";
 import { UsersService } from "./users/users.service";
 import { PostsService } from "./posts/posts.service";
 import { CommentsService } from "./comments/comments.service";
 import { QueryService } from "./sup-services/query/query.service";
+import { Response } from "express";
 
 @Controller()
 export class AppController {
@@ -16,11 +17,12 @@ export class AppController {
     ) {}
 
     @Delete("testing/all-data")
-    async deleteAll(): Promise<void> {
+    async deleteAll(@Res() res: Response): Promise<void> {
         await this.blogService.testingDelete();
         await this.postsService.testingDelete();
         await this.usersService.testingDelete();
         await this.queryService.testingDelete();
         await this.commentsService.testingDelete();
+        res.sendStatus(HttpStatus.NO_CONTENT);
     }
 }
