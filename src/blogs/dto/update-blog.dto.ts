3213d3befbd9af7eecdff6c239c3c2ab219d133b
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-06-07 16:02:32 +0300

    hw26

--- src/blogs/dto/update-blog.dto.ts
+++ src/blogs/dto/update-blog.dto.ts
@@ -1,8 +1,25 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateBlogDto } from "./create-blog.dto";
+import { IsNotEmpty, IsString, Matches, MaxLength, Validate } from "class-validator";
+import { TrimStringValidator } from "../../pipes/validation.pipes";
 
 export class UpdateBlogDto extends PartialType(CreateBlogDto) {
+    @MaxLength(15)
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
     readonly name: string;
+
+    @MaxLength(500)
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
     readonly description: string;
+
+    @Matches(/^(https?:\/\/)?([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[^\s]*)?$/)
+    @MaxLength(100)
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
     readonly websiteUrl: string;
 }
--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,9 +1,30 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreatePostDto } from "./create-post.dto";
+import { IsNotEmpty, IsString, MaxLength, Validate } from "class-validator";
+import { IsBlogIdCheck, TrimStringValidator } from "../../pipes/validation.pipes";
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
-    // readonly title: string;
-    // readonly shortDescription: string;
-    // readonly content: string;
-    // readonly blogId: string;
+    @MaxLength(30)
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
+    readonly title: string;
+
+    @MaxLength(100)
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
+    readonly shortDescription: string;
+
+    @MaxLength(1000)
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
+    readonly content: string;
+
+    @IsBlogIdCheck({ message: "BlogId has incorrect value. (BlogId not found)" })
+    @IsString()
+    @IsNotEmpty()
+    @Validate(TrimStringValidator)
+    readonly blogId: string;
 }
