d6a4cf093e7212a44c47d61e53cccbd53c4e1b93
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-06-07 10:18:20 +0300

    hw21

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,4 +1,4 @@
-import { IContent } from "../interface/comment.interface";
+//import { IContent } from "../interface/comment.interface";
 import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateCommentDto {
@@ -8,7 +8,7 @@ export class CreateCommentDto {
     @MaxLength(300)
     readonly content: string;
 
-    constructor(props: IContent) {
-        this.content = props.content;
-    }
+    // constructor(props: IContent) {
+    //     this.content = props.content;
+    // }
 }
