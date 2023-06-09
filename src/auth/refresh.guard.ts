37f03a283aba9e84d56bb88c13c993517454afd2
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-06-09 10:27:55 +0300

    hw42(debug 4)

--- src/auth/refresh.guard.ts
+++ src/auth/refresh.guard.ts
@@ -1,26 +1,5 @@
-import {Inject, Injectable} from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import {UsersRepository} from "../users/users.repository";
-import {UserModel} from "../users/schema/user.schema";
 
 @Injectable()
-export class RefreshGuard extends AuthGuard("refresh") {
-    // constructor(@Inject("userRepository") private readonly userRepository: UsersRepository) {
-    //     super();
-    //     this.userRepository = new UsersRepository(UserModel);
-    // }
-    //
-    // async validate(value: string) {
-    //     try {
-    //         const user = await this.userRepository.findUserByParam(value);
-    //         if (user) {
-    //             throw new Error();
-    //         }
-    //     } catch (error) {
-    //         if (error instanceof Error) {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-}
+export class RefreshGuard extends AuthGuard("refresh") {}
